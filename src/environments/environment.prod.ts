1867056eb5efdce0b54e1884c92e714f6ce53f25
dpolo-jamar <44912504+DanielPoloC@users.noreply.github.com>
2022-03-31 01:12:18 -0500

    base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  domain: 'https://prueba-cidenet-back.herokuapp.com',
+  apis: {
+    obtener: '/v1/empleados/obtener',
+    crear: '/v1/empleados/crear',
+    modificar: '/v1/empleados/modificar',
+    eliminar: '/v1/empleados/eliminar'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  domain: 'https://prueba-cidenet-back.herokuapp.com',
+  apis: {
+    obtener: '/v1/empleados/obtener',
+    crear: '/v1/empleados/crear',
+    modificar: '/v1/empleados/modificar',
+    eliminar: '/v1/empleados/eliminar'
+  }
 };
 
 /*
